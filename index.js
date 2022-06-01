8ce9fb9111cb6226f386b29df20cdd9095e053a5
= <neymegen50@gmail.com>
2022-06-01 22:58:59 +0300

    init files

---
+++ index.js
@@ -0,0 +1,25 @@
+const baseUrl = 'https://629649eb810c00c1cb72fc33.mockapi.io/api/v1/users';
+const buttonSubmit = document.querySelector('.submit-button');
+const formElem = document.querySelector('.login-form');
+
+function userData() {
+  const formData = Object.fromEntries(new FormData(formElem));
+  if (formElem.reportValidity() === true) {
+    buttonSubmit.disabled = false;
+  }
+  return JSON.stringify(formData);
+}
+
+function createUser(userData) {
+  return fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(userData),
+  }).then((response) => response.body);
+}
+buttonSubmit.addEventListener('click', createUser);
+
+const input = document.querySelector('input');
+input.value = '';
